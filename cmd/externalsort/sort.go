d707cdedb9edb0566db7448e62b3cdc4c3c84f72
caidongfang <cadofa@163.com>
2019-02-18 17:54:43 +0800

    添加并发版pipeline

--- cmd/externalsort/sort.go
+++ cmd/externalsort/sort.go
@@ -5,10 +5,11 @@ import (
 	"imooc.com/ccmouse/learngo/pipeline"
 	"bufio"
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	p := createPipeline("large.in", 800000000, 100)
+	p := createNetworkPipeline("large.in", 800000000, 4)
 	writeToFile(p, "large.out")
 	printFile("large.out")
 }
@@ -39,6 +40,36 @@ func writeToFile(p <-chan int, filename string) {
 	writer := bufio.NewWriter(file)
 	defer writer.Flush()
 	pipeline.WriterSink(writer, p)
+}
+
+func createNetworkPipeline(filename string, fileSize, chunkCount int) <-chan int {
+	chunkSize := fileSize / chunkCount
+	pipeline.Init()
+
+	sortAddr := []string{}
+	for i := 0; i < chunkCount; i++{
+		file, err := os.Open(filename)
+		if err != nil {
+			panic(err)
+		}
+
+		file.Seek(int64(i * chunkSize), 0)
+
+		source := pipeline.ReaderSource(
+			bufio.NewReader(file), chunkSize)
+
+		addr := ":" + strconv.Itoa(7000 + i)
+		pipeline.NetworkSink(addr, pipeline.InMemSort(source))
+		sortAddr = append(sortAddr, addr)
+	}
+
+	sortResults := []<-chan int{}
+	for _, addr := range sortAddr {
+		sortResults = append(sortResults,
+			pipeline.NetworkSource(addr))
+	}
+
+	return pipeline.MergeN(sortResults...)
 }
 
 func createPipeline(filename string, fileSize, chunkCount int) <-chan int {
